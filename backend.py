1b55f53822a97a7d1fa54621b15c342ee67cb7cb
Chiraagkv <chiraagkv@gmail.com>
2023-11-04 11:48:41 +0530

    matching working

--- backend.py
+++ backend.py
@@ -1,16 +1,12 @@
 import pandas as pd
-rest_df = pd.read_csv("rests.csv")
-cust_df = pd.read_csv("custs.csv")
+
 def create_dict(df):
     names = df["Name"]
     cuisines = df["Cuisines"]
     dictionary = {}
     for n, i in enumerate(names):
-        dictionary[i] = cuisines[n]
+        dictionary[i] = list(cuisines[n].split(" "))
     return dictionary
-
-cust_dict = create_dict(cust_df)
-rest_dict = create_dict(rest_df)
 
 def match(cuisines_c, cuisines_r):
      count = 0
@@ -20,19 +16,35 @@ def match(cuisines_c, cuisines_r):
             if i == j:
                 count += 1
                 cc.append(i)
-     if len(cc) >= 3:
-        return cc, True
+     if len(cc) >= 2: # Change as required
+        return True
      else:
-        return cc, False
+        return False
 
-cu_re = {}
-for cu, cu_c in cust_dict.items():
-    for re, re_c in rest_dict.items():
-        stat, cc = match(cu_c, re_c)
-        if stat:
-            cu_re[cu] = re
-
-def create_availability_dict(rest_df):
+def create_availability_list(rest_df,  required):
     rests = rest_df["Name"]
     seats = rest_df["Seats"]
+    available = []
+    for n, i in enumerate(seats):
+        if i >= required:
+            available.append(rests[n])
+    return available
 
+
+rest_df = pd.read_csv("rests.csv")
+cust_df = pd.read_csv("custs.csv")
+cu_re = {}
+cust_dict = create_dict(cust_df)
+rest_dict = create_dict(rest_df)
+print(rest_dict, cust_dict)
+for cu, cu_c in cust_dict.items():
+    required = int(input("Enter number of seat required: "))
+    available = create_availability_list(rest_df, required)
+    res = []
+    for re, re_c in rest_dict.items():
+        if re in available:
+            stat = match(cu_c, re_c)
+            if stat:
+                res.append(re)
+    cu_re[cu] = res
+print(cu_re)
